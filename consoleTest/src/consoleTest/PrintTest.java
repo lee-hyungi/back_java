ffceddf2e5612d93830096b26a0a7765e4eca2cb
lee-hyungi <gusrl9747@gmail.com>
2025-08-14 17:45:08 +0900

    day01

---
+++ consoleTest/src/consoleTest/PrintTest.java
@@ -0,0 +1,10 @@
+package consoleTest;
+
+public class PrintTest {
+	public static void main(String[] args) {
+//	    System.out.print(): 줄바꿈 없이 값을 콘솔에 출력
+//	    System.out.println(): 문자열을 출력하고 줄바꿈을 한다.
+		System.out.print("이현기\"환영\"합니다\n");
+		System.out.print(20);
+	}
+}
---
+++ variable/src/variableTest/VariableTest.java
@@ -0,0 +1,22 @@
+package variableTest;
+// 왼족에 타입이 있으면 저장공간 / 왼쪽에 대입연산자 값
+public class VariableTest {
+	public static void main(String[] args) {
+// 		초기값
+//		정수(int): 0
+//		실수(double): 0.0 
+//		문자(char): ' '
+//		문자열(String): null, ""
+
+//		기본 자료형
+		int num1 = 10;
+		double num2 = 10.7;
+		float num3 = 10.7F;
+		char data1 = '가';
+		long num4 = 10L;
+		
+//		클래스형 자료형 (앞글자 대문자)
+		String hello = "안녕";
+		
+	}
+}
